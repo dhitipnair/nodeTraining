4625f52b7bb37dbebf2235d82338c934abcab873
dhitipnair <124810830+dhitipnair@users.noreply.github.com>
2023-02-23 00:41:49 +0530

    Add files via upload

---
+++ ReactAssesment/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import ProductInfo from './component/ProductInfo';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Items Catalogue </h1><br/>
+     <ProductInfo />
+    </div>
+  );
+}
+
+export default App;
